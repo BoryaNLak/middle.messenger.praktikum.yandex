c84dbc5e19fbd7fe484ae28e460cffd27c89bc26
boris <borislakushev@yandex.ru>
2022-08-02 07:28:06 +0300

    create: dynamic port for heroku

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
-const port = process.env.PORT || PORT;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/dist/`));
 
@@ -13,5 +12,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Run app on ${port} port`);
+  console.log(`Run app on ${PORT} port`);
 });
